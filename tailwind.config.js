4e59400409c229e85cf6414571e6e3abdb43afaa
Jonathan Ceasar Medinae <Jonathan.C.Medina.x@gmail.com>
2023-08-24 14:15:59 -0700

    did a lot of stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  darkMode:"class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
   ],
   theme: {
     extend: {
       fontFamily: {
-        burtons: "burtons",
-        poppins: ["Poppins", "sans-serif"],
-      },
+        burtons:"burtons",
+      }
     },
   },
   plugins: [],
-};
+}

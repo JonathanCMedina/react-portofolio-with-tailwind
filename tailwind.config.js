76d261c7f59c4bc21e84490351beb02f554f06c3
Jonathan Ceasar Medinae <Jonathan.C.Medina.x@gmail.com>
2024-01-30 14:59:04 -0700

    not trying flowbite carousel lol they need node.js and some other bs, trying out a carousel specifically for nextjs and tailwind, also deleted carousel2.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode:"class",
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
   ],
   theme: {
     extend: {
       fontFamily: {
-        burtons:"burtons",
-      }
+        burtons: "burtons",
+      },
     },
   },
   plugins: [],
-}
+};

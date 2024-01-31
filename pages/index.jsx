be6826e71b9de3315e7e6385d3cbf01cf2f27d2c
Jonathan Ceasar Medinae <Jonathan.C.Medina.x@gmail.com>
2024-01-31 13:58:30 -0700

    scrapped carousel2, keeping carousel.jsx since it worked somewhat, but pivoting to a card system, maybe like 4-5 per project entry of some of the pages? idk man im tired

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,17 +8,7 @@ import python_logo from '../public/python_logo.png';
 import cubes from '../public/cubes.png';
 import project_alpha_1 from '../public/project_alpha_1.png';
 import project_alpha_2 from '../public/project_alpha_2.png';
-import CarCar_Automobile_List from '../public/CarCar/CarCar_Automobile_List.png';
-import CarCar_Automobiles_Sold from '../public/CarCar/CarCar_Automobile_List.png';
-// import CarCar_Automobile_List from '../public/CarCar/CarCar_Automobile_List.png'
-// import CarCar_Automobile_List from '../public/CarCar/CarCar_Automobile_List.png'
-// import CarCar_Automobile_List from '../public/CarCar/CarCar_Automobile_List.png'
-// import CarCar_Automobile_List from '../public/CarCar/CarCar_Automobile_List.png'
-// import CarCar_Automobile_List from '../public/CarCar/CarCar_Automobile_List.png'
-// import CarCar_Automobile_List from '../public/CarCar/CarCar_Automobile_List.png'
 import { useState, useEffect } from "react";
-import ContactForm from "./contactForm";
-
 
 
 export default function Home() {
@@ -334,81 +324,15 @@ export default function Home() {
             Please take a look at the Readme file as we crafted it very
             meticulously! 😁
           </p>
+          <div>
+
+          </div>
             {/* pictures section for CarCar */}
 {/* <div className="flex flex-col gap-10 lg:flex-row lg_flex-wrap">
   <div className="basis-1/3 flex-1">
     <Image src={project_alpha_1} alt="..." width={"100%"} height={"100%"} layout="responsive"/>
   </div>
 </div> */}
-
-
-<div id="default-carousel" className="relative w-full" data-carousel="slide">
-    {/* <!-- Carousel wrapper --> */}
-    <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-         {/* <!-- Item 1 --> */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image src={project_alpha_1} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."
-              width={100}
-              height={100}
-              layout="responsive"
-              />
-        </div>
-        {/* <!-- Item 2 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image src={project_alpha_2} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."
-                width={100}
-                height={100}
-                />
-        </div> */}
-        {/* <!-- Item 3 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image src="/docs/images/carousel/carousel-3.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."
-                width={"100%"}
-                height={"100%"}
-                />
-        </div> */}
-        {/* <!-- Item 4 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image src="/docs/images/carousel/carousel-4.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."
-                width={"100%"}
-                height={"100%"}
-                />
-        </div> */}
-        {/* <!-- Item 5 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image src="/docs/images/carousel/carousel-5.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."
-                width={"100%"}
-                height={"100%"}
-                />
-        </div> */}
-    </div>
-    {/* <!-- Slider indicators --> */}
-    <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-    </div>
-    {/* <!-- Slider controls --> */}
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
-</div>
-
 
 
           {/* Project Alpha */}
